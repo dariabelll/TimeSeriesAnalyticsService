15b79043233123bef7bf9d0145d9f980b0bf2056
daria <d4riyabel@yandex.ru>
2026-02-27 01:54:03 +0300

    Implement validation

---
+++ TimeSeriesAnalyticsService.Application/Validation/TimeSeriesValidator.cs
@@ -0,0 +1,21 @@
+using FluentValidation;
+using TimeSeriesAnalyticsService.Domain.Models;
+
+namespace TimeSeriesAnalyticsServices.Application.Validation;
+
+public sealed class TimeSeriesValidator: AbstractValidator<TimeSeriesValue>
+{
+    private static readonly DateTimeOffset MinAllowedDateUtc = new(new DateTime(2000, 1, 1, 0, 0, 0, DateTimeKind.Utc));
+
+    public TimeSeriesValidator()
+    {
+        RuleFor(x => x.Date).GreaterThanOrEqualTo(MinAllowedDateUtc).WithMessage("Date must be greater than 2000-01-01");
+
+        RuleFor(x => x.Date).LessThanOrEqualTo(DateTimeOffset.UtcNow).WithMessage("Date must be less than current date");
+
+        RuleFor(x => x.ExecutionTimeSeconds).GreaterThanOrEqualTo(0).WithMessage("Execution time must be greater than 0");
+
+        RuleFor(x => x.Value).GreaterThanOrEqualTo(0).WithMessage("Value must be greater than 0");
+    }
+
+}
