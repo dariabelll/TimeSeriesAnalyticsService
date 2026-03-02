8def5f6a4ba4957c85395b6fccfb85b6296c6814
daria <d4riyabel@yandex.ru>
2026-03-02 03:39:27 +0300

    Add migrations

---
+++ TimeSeriesAnalyticsService.Infrastructure.DataAccess.EntityFramework/Migrations/20260302003752_Initial.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace TimeSeriesAnalyticsService.Infrastructure.DataAccess.EntityFramework.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Results",
+                columns: table => new
+                {
+                    FileName = table.Column<string>(type: "text", nullable: false),
+                    DeltaSeconds = table.Column<double>(type: "double precision", nullable: false),
+                    FirstStart = table.Column<DateTimeOffset>(type: "timestamptz", nullable: false),
+                    AverageExecutionTimeSeconds = table.Column<double>(type: "double precision", nullable: false),
+                    AverageValue = table.Column<double>(type: "double precision", nullable: false),
+                    MedianValue = table.Column<double>(type: "double precision", nullable: false),
+                    MaxValue = table.Column<double>(type: "double precision", nullable: false),
+                    MinValue = table.Column<double>(type: "double precision", nullable: false),
+                    RowCount = table.Column<int>(type: "integer", nullable: false),
+                    UpdatedAt = table.Column<DateTimeOffset>(type: "timestamptz", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Results", x => x.FileName);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Values",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Date = table.Column<DateTimeOffset>(type: "timestamptz", nullable: false),
+                    ExecutionTimeSeconds = table.Column<double>(type: "double precision", nullable: false),
+                    Value = table.Column<double>(type: "double precision", nullable: false),
+                    FileName = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Values", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Results_AverageExecutionTimeSeconds",
+                table: "Results",
+                column: "AverageExecutionTimeSeconds");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Results_AverageValue",
+                table: "Results",
+                column: "AverageValue");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Results_FirstStart",
+                table: "Results",
+                column: "FirstStart");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Values_FileName_Date",
+                table: "Values",
+                columns: new[] { "FileName", "Date" });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Results");
+
+            migrationBuilder.DropTable(
+                name: "Values");
+        }
+    }
+}
